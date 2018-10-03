faa176426d879654a5aa9ed83b4372e9c016e1b8
sayanmanik <sayanmanik.sm@gmail.com>
2018-10-03 20:44:14 +0530

    Initial commit

---
+++ Desktop/sayan/android projects/Kindo/app/src/main/java/com/example/sayan/kindo/RainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sayan.kindo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rain);
+    }
+}
---
+++ Desktop/sayan/android projects/Kindo/app/src/main/java/com/example/sayan/kindo/Service/MusicService.java
@@ -0,0 +1,67 @@
+package com.example.sayan.kindo.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.example.sayan.kindo.R;
+
+import java.io.IOException;
+
+/**
+ * Created by 1605476 and 02-Oct-18
+ **/
+
+public class MusicService extends Service
+{
+
+    MediaPlayer player;
+   // MusicBinder binder=new MusicBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+  /*  public class MusicBinder extends Binder
+    {
+         public MusicService getService()
+         {
+             return MusicService.this;
+         }
+    }*/
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player=MediaPlayer.create(this,R.raw.eine);
+        player.setLooping(true);
+        player.setVolume(100,100);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        player.start();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+
+        super.onStart(intent, startId);
+    }
+
+    
+    @Override
+    public void onDestroy()
+    {
+        player.stop();
+        player.release();
+        super.onDestroy();
+    }
+}
